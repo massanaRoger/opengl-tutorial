7a939113df2035868280663c8ddd56535d13f7de
Roger Massana <rogermassana2002@gmail.com>
2024-05-24 16:08:00 +0200

    Hello triangle exercise 1 done

--- src/main.cpp
+++ src/main.cpp
@@ -19,30 +19,25 @@ const char *fragmentShaderSource = "#version 330 core\n"
 
 unsigned int vertexInput()
 {
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };   
 
-    unsigned int VBO, VAO, EBO;
+    float vertices[] = {
+        -1.0f, -0.5f, 0.0f, // left  
+        0.0f, -0.5f, 0.0f, // right 
+        -0.5f,  0.5f, 0.0f,  // top   
+        0.0f, -0.5f, 0.0f, // left  
+        1.0f, -0.5f, 0.0f, // right 
+        0.5f,  0.5f, 0.0f  // top   
+    }; 
+
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -163,7 +158,7 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
         glfwPollEvents();    
