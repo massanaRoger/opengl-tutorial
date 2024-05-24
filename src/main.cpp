4577493b42919a5a2eb370f8252772f995afbd30
Roger Massana <rogermassana2002@gmail.com>
2024-05-24 12:06:24 +0200

    Changed project name in CMake

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
