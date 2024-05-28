28a6872e109d5d49e4ff04f8a9035dd8954ac8ef
Roger Massana <rogermassana2002@gmail.com>
2024-05-28 15:11:09 +0200

    Transformations exercise 1: first translate then rotate

--- src/main.cpp
+++ src/main.cpp
@@ -168,8 +168,8 @@ int main() {
     shader.use();
 
     glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
     trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
 
     unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
     glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
