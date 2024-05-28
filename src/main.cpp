6bde8102ac8dd0db2072b999aa79816922fd57c2
Roger Massana <rogermassana2002@gmail.com>
2024-05-28 21:12:54 +0200

    Removed .cache folder from git

--- src/main.cpp
+++ src/main.cpp
@@ -195,7 +195,7 @@ int main() {
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.8f, 0.2f, 0.5f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glActiveTexture(GL_TEXTURE0);
