e395a22a57eeea868958e2ac5edffe1ac71ed741
Roger Massana <rogermassana2002@gmail.com>
2024-05-28 13:42:26 +0200

    Added math library

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 unsigned int vertexInput()
 {
