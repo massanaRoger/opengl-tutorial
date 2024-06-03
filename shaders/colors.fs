15bdd74614b96d5e5325169be944ae28bf5ae1e5
Roger Massana <rogermassana2002@gmail.com>
2024-06-03 17:20:32 +0200

    Exercise done

--- shaders/colors.fs
+++ shaders/colors.fs
@@ -3,11 +3,10 @@ out vec4 FragColor;
   
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec3 LightPos;
 
 void main()
 {
@@ -15,13 +14,12 @@ void main()
     vec3 ambient = ambientStrength * lightColor;
 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-
+    vec3 lightDir = normalize(LightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(-FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * lightColor;
--- shaders/colors.vs
+++ shaders/colors.vs
@@ -2,16 +2,20 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
+out vec3 Normal;
+out vec3 FragPos;
+out vec3 LightPos;
+
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 Normal;
-out vec3 FragPos;
+uniform vec3 lightPos;
 
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = aNormal;
+    FragPos = vec3(view * model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(view * model))) * aNormal;
+    LightPos = vec3(view * vec4(lightPos, 1.0));
 }
