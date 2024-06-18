5a8bd9910f8fb9ac6b0e7c435538a1273d2e9f8f
massanaRoger <94066091+massanaRoger@users.noreply.github.com>
2024-06-18 18:10:36 +0200

    Mesh class working

---
+++ src/Mesh.h
@@ -0,0 +1,37 @@
+#ifndef MESH_H
+#define MESH_H
+#include <string>
+#include <vector>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
+#include "Shader.hpp"
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+class Mesh {
+public:
+    // mesh data
+    std::vector<Vertex>       vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture>      textures;
+
+    Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices, const std::vector<Texture>& textures);
+    void Draw(Shader &shader);
+private:
+    //  render data
+    unsigned int m_VAO, m_VBO, m_EBO;
+
+    void setupMesh();
+};
+
+#endif
