702df9888d6f93b4139c081523b75aac2ff5898d
massanaRoger <94066091+massanaRoger@users.noreply.github.com>
2024-06-18 20:54:16 +0200

    Process vertex positions, normals, and texture coords

--- src/Model.cpp
+++ src/Model.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Model.h"
+#include "Mesh.h"
 #include "assimp/Importer.hpp"
 #include <iostream>
 #include <assimp/postprocess.h>
@@ -27,4 +28,38 @@ void Model::loadModel(std::string path)
     directory = path.substr(0, path.find_last_of('/'));
 
     processNode(scene->mRootNode, scene);
+}
+
+Mesh processMesh(aiMesh *mesh, const aiScene *scene)
+{
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    for(unsigned int i = 0; i < mesh->mNumVertices; i++)
+    {
+        Vertex vertex;
+        // process vertex positions, normals and texture coordinates
+        vertex.Position.x = mesh->mVertices[i].x;
+        vertex.Position.y = mesh->mVertices[i].y;
+        vertex.Position.z = mesh->mVertices[i].z;
+
+        vertex.Normal.x = mesh->mNormals[i].x;
+        vertex.Normal.y = mesh->mNormals[i].y;
+        vertex.Normal.z = mesh->mNormals[i].z;
+
+        if (mesh->mTextureCoords[0]) 
+        {
+            glm::vec2 vec;
+            vec.x = mesh->mTextureCoords[0][i].x;
+            vec.y = mesh->mTextureCoords[0][i].y;
+            vertex.TexCoords = vec;
+        } 
+        else 
+        {
+            vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+        }
+
+        vertices.push_back(vertex);
+    }
 }
